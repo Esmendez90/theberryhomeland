cd5bee0350bbab16aa054c7bac9c37d7c5fabd35
esmendez90 <edmendezn@gmail.com>
2024-01-11 15:05:26 -0500

    edited video

--- script.js
+++ script.js
@@ -2,10 +2,4 @@ document.getElementById("goTo-gallery-btn").onclick = function () {
     location.href = "https://esmendez90.github.io/theberryhomeland/gallery.html";
 };
 
-let interval = setInterval(function(){
-    let countForVideo = document.getElementById('vid').readyState;
-    if(countForVideo == 4){
-      document.getElementById('vid').play();
-      clearInterval(interval);
-    }
-  },2000);
+
