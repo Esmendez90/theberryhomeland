84e469fbf8dd644c985fe78bf1b18325ffad8665
esmendez90 <edmendezn@gmail.com>
2024-01-11 14:55:40 -0500

    added video

--- script.js
+++ script.js
@@ -2,4 +2,10 @@ document.getElementById("goTo-gallery-btn").onclick = function () {
     location.href = "https://esmendez90.github.io/theberryhomeland/gallery.html";
 };
 
-
+let interval = setInterval(function(){
+    let countForVideo = document.getElementById('vid').readyState;
+    if(countForVideo == 4){
+      document.getElementById('vid').play();
+      clearInterval(interval);
+    }
+  },2000);
