536b8de961b9247a8e1d9699fec7581df8adf260
esmendez90 <edmendezn@gmail.com>
2024-01-11 14:28:04 -0500

    added btn

--- script.js
+++ script.js
@@ -1,14 +1,5 @@
-// let stopActiveClass = document.getElementsByClassName("snapshot-container")[0].children[0].children[0];
-
-// $(window).resize(function() {
-//     let screenWidth = window.screen.width
-//     console.log(screenWidth);
-    
-//     if (screenWidth >= 992) {
-//         console.log("Greater than or = ", screenWidth);
-//         $(stopActiveClass).removeClass("active");
-//     }
-//   });
-
+document.getElementById("goTo-gallery-btn").onclick = function () {
+    location.href = "https://esmendez90.github.io/theberryhomeland/gallery.html";
+};
 
 
