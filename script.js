b4fb85db959d3c64a02bd04eccc6cfe1f153af7f
esmendez90 <edmendezn@gmail.com>
2024-01-11 12:37:44 -0500

    added images

---
+++ script.js
@@ -0,0 +1,14 @@
+// let stopActiveClass = document.getElementsByClassName("snapshot-container")[0].children[0].children[0];
+
+// $(window).resize(function() {
+//     let screenWidth = window.screen.width
+//     console.log(screenWidth);
+    
+//     if (screenWidth >= 992) {
+//         console.log("Greater than or = ", screenWidth);
+//         $(stopActiveClass).removeClass("active");
+//     }
+//   });
+
+
+
