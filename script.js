e38dfd61c8f403be872fc302acf7233f5f17e83c
esmendez90 <edmendezn@gmail.com>
2024-01-17 16:46:16 -0500

    edited btn function

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 document.getElementById("goTo-gallery-btn").onclick = function () {
-    location.href = "https://esmendez90.github.io/theberryhomeland/gallery.html";
+        location.href = location.href + "/gallery.html";
 };
 
+// let el = document.getElementsByClassName("snapshot-inner-container")[0];
+// console.log(el);
+// function isInViewport(el) {
+
+//     var rect = el.getBoundingClientRect();
+//     var html = document.documentElement;
+//     console.log(rect,html);
+//   }
+
+//   isInViewport();
 
