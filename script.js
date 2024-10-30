72e37bbf8d38a1b77bab39bd6c89a45872503557
Esteban Mendez <estebanmendez@Estebans-MacBook-Pro.local>
2024-10-30 11:58:43 -0400

    modified gallery page. added buttons to access specific pictures. added js functionality like onclick events

--- script.js
+++ script.js
@@ -1,20 +1,41 @@
 const preLoaderImg = document.getElementsByClassName("onload-img");
 const introTextFadeIn = document.getElementsByClassName("intro-text");
 const elements = document.querySelectorAll(".display-element");
+const abacaBtn = document.getElementById("abaca-container");
+const cacaoBtn = document.getElementById("cacao-container");
 
-if ((preLoaderImg) && (introTextFadeIn) ){
+const abacaPhotosContainer =
+  document.getElementsByClassName("gallery-item");
+const cacaoPhotosContainer =
+  document.getElementsByClassName("gallery-item-2");
+
+if (abacaBtn){
+  abacaBtn.addEventListener("click", function (e) {
+  e.preventDefault();
+  abacaPhotosContainer[0].style.display = "grid";
+  cacaoPhotosContainer[0].style.display = "none";
+});
+} 
+
+if (cacaoBtn){
+
+cacaoBtn.addEventListener("click", function (e) {
+  e.preventDefault();
+  cacaoPhotosContainer[0].style.display = "grid";
+  abacaPhotosContainer[0].style.display = "none";
+});
+}
+
+if (preLoaderImg && introTextFadeIn) {
   function preloaderFunction() {
     $(preLoaderImg[0]).addClass("fade-out");
     $(introTextFadeIn[0]).addClass("fade-in");
   }
 }
 
-
 function isInViewport(element) {
   const rect = element.getBoundingClientRect();
-  return (
-    rect.y <= 850
-  );
+  return rect.y <= 850;
 }
 
 window.addEventListener("scroll", () => {
