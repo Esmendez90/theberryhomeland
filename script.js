bbbc6f7e9341ad4982b95cda6d5ca471030063df
Esteban Mendez <estebanmendez@Estebans-MacBook-Pro.local>
2025-02-14 14:54:56 -0500

    Enhance UI: Add button with background image, relocate section, and adjust text styling.

--- script.js
+++ script.js
@@ -2,28 +2,38 @@ const preLoaderImg = document.getElementsByClassName("onload-img");
 const introTextFadeIn = document.getElementsByClassName("intro-text");
 const elements = document.querySelectorAll(".display-element");
 const abacaBtn = document.getElementById("abaca-container");
+const coffeeBtn = document.getElementById("coffee-container");
 const cacaoBtn = document.getElementById("cacao-container");
 
-const abacaPhotosContainer =
-  document.getElementsByClassName("gallery-item");
-const cacaoPhotosContainer =
-  document.getElementsByClassName("gallery-item-2");
+const abacaPhotosContainer = document.getElementsByClassName("gallery-item");
+const coffeePhotosContainer = document.getElementsByClassName("gallery-item-2");
+const cacaoPhotosContainer = document.getElementsByClassName("gallery-item-3");
 
-if (abacaBtn){
+if (abacaBtn) {
   abacaBtn.addEventListener("click", function (e) {
-  e.preventDefault();
-  abacaPhotosContainer[0].style.display = "grid";
-  cacaoPhotosContainer[0].style.display = "none";
-});
-} 
+    e.preventDefault();
+    abacaPhotosContainer[0].style.display = "grid";
+    coffeePhotosContainer[0].style.display = "none";
+    cacaoPhotosContainer[0].style.display = "none";
+  });
+}
 
-if (cacaoBtn){
+if (coffeeBtn) {
+  coffeeBtn.addEventListener("click", function (e) {
+    e.preventDefault();
+    abacaPhotosContainer[0].style.display = "none";
+    coffeePhotosContainer[0].style.display = "grid";
+    cacaoPhotosContainer[0].style.display = "none";
+  });
+}
 
-cacaoBtn.addEventListener("click", function (e) {
-  e.preventDefault();
-  cacaoPhotosContainer[0].style.display = "grid";
-  abacaPhotosContainer[0].style.display = "none";
-});
+if (cacaoBtn) {
+  cacaoBtn.addEventListener("click", function (e) {
+    e.preventDefault();
+    abacaPhotosContainer[0].style.display = "none";
+    coffeePhotosContainer[0].style.display = "none";
+    cacaoPhotosContainer[0].style.display = "grid";
+  });
 }
 
 if (preLoaderImg && introTextFadeIn) {
@@ -49,5 +59,3 @@ window.addEventListener("scroll", () => {
 });
 
 window.onload = preloaderFunction;
-
-
